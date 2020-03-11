eee1c8034e2b65825fc8ea352c3ab8dccd832ae0
fmoreirati <fabianomoreira.ti@gmail.com>
2020-03-11 11:32:51 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  base64textString: string;
+
+  selectFile(event) {
+    var files = event.target.files;
+    var file = files[0];
+
+    if (files && file) {
+      var reader = new FileReader();
+
+      reader.onload = this.handleFile.bind(this);
+
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  handleFile(event) {
+    var binaryString = event.target.result;
+    this.base64textString = 'data:image/jpeg;base64,' + btoa(binaryString);
+    console.log(btoa(binaryString));
+  }
+}
